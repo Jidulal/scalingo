ec9ed28a86d14b90637dce7c1b7885058e0c9cf5
Jidu Lal <jidulalvm28@gmail.com>
2018-08-28 12:00:13 +0300

    db details

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php
-	$url = parse_url(getenv('SCALINGO_MYSQL_URL'));
+	$url = parse_url(getenv('SCALINGO_INFLUX_URL'));
 
 	
 	$db = array(
-		'dsn'	=> getenv('SCALINGO_MYSQL_URL'),
+		'dsn'	=> getenv('SCALINGO_INFLUX_URL'),
 		'hostname' => $url['host'] . ':' . $url['port'],
 		'username' => $url['user'],
 		'password' => $url['pass'],
-		'database' => substr($url['path'], 1),
-		'dbdriver' => 'mysqli'
+		'database' => substr($url['path'], 1)
 	);
 	echo "<pre>"; print_r($db); echo "</pre>";
 ?>
