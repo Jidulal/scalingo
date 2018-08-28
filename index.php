a634803f63cb28466e30586164ec669a47c21934
Jidu Lal <jidulalvm28@gmail.com>
2018-08-28 11:50:07 +0300

    Index page

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
+echo phpinfo();
 	echo '<html><head><title>Sample Scalingo App</title></head><body><table border="1"><tr><th>Adding InfluxDB to your app</th></tr><tr><td>You can add the InfluxDB addon through the Dashboard or through the command line interface. The capacity of your database is elastic, you will be able to upgrade it later.</td></tr></table></body></html>';
 ?>
