13264179e7cb30615b0cb7c9f3118ceba21e9cb1
Jidu Lal <jidulalvm28@gmail.com>
2018-09-06 08:22:36 +0300

    Name chaanged

--- index.php
+++ index.php
@@ -1,45 +1,68 @@
 <?php
-	$url = parse_url(getenv('SCALINGO_INFLUX_URL'));
+/**
+ * @author Stephen "TheCodeAssassin" Hoogendijk
+ */
+require 'vendor/autoload.php';
 
-	
-	$db = array(
-		'dsn'	=> getenv('SCALINGO_INFLUX_URL'),
-		'hostname' => $url['host'] . ':' . $url['port'],
-		'username' => $url['user'],
-		'password' => $url['pass'],
-		'database' => substr($url['path'], 1)
-	);
-	//echo "<pre>"; print_r($db); echo "</pre><br>";
-	
-	
-	
-	require 'vendor/autoload.php';
-	
-	$client = new \InfluxDB\Client($url['host'], $url['port']);
-	echo "<pre>"; print_r($client); echo "</pre><br>";
-	
-	$listdb = $client->listDatabases();
-	
-	echo "<pre>"; print_r($listdb); echo "</pre><br>";
-	
-	$database = $client->selectDB(substr($url['path'], 1));
-	echo "<pre>"; print_r($database); echo "</pre><br>";
+// vagrant ip
+$host = '192.168.33.10';
+$url = parse_url(getenv('SCALINGO_INFLUX_URL'));
 
-	
-	$points = array(
-		new Point('test_metric',0.64, ['host' => 'server01', 'region' => 'us-west'], ['cpucount' => 10], 1435255849	),
-		new Point('test_metric',0.84, ['host' => 'server01', 'region' => 'us-west'], ['cpucount' => 10], 1435255849	)
-	);
-	
-	$inser = $database->writePoints($points, Database::PRECISION_SECONDS);
-	
-	echo "<pre>"; print_r($inser); echo "</pre><br>";
-	
-	
-	
-	$result = $database->query('select * from test_metric');
+function randFloat($min, $max)
+{
+    $range = $max-$min;
+    $num = $min + $range * mt_rand(0, 32767)/32767;
 
-	$datas = $result->getPoints();
-	
-	echo "<pre>"; print_r($datas); echo "</pre><br>";
-?>
+    $num = round($num, 4);
+
+    return ((float) $num);
+}
+
+$client = new \InfluxDB\Client($url['host']);
+
+$database = $client->selectDB(substr($url['path'], 1));
+
+/* if ($database->exists()) {
+    $database->drop();
+}
+
+$database->create(new \InfluxDB\Database\RetentionPolicy('test', '12w', 1, true)); */
+
+
+$start = microtime(true);
+
+$countries = ['nl', 'gb', 'us', 'be', 'th', 'jp', 'nl', 'sa'];
+$colors = ['orange', 'black', 'yellow', 'white', 'red', 'purple'];
+$points = [];
+
+for ($i=0; $i < 1000; $i++) {
+    $points[] = new \InfluxDB\Point(
+        'flags',
+        randFloat(1, 999),
+        ['country' => $countries[array_rand($countries)]],
+        ['color' => $colors[array_rand($colors)]],
+        (int) shell_exec('date +%s%N')+mt_rand(1,1000)
+    );
+};
+
+// insert the points
+$database->writePoints($points);
+
+$end = microtime(true);
+
+$country = $countries[array_rand($countries)];
+$color = $colors[array_rand($colors)];
+
+$results = $database->query("SELECT * FROM flags WHERE country = '$country' LIMIT 5")->getPoints();
+$results2 = $database->query("SELECT * FROM flags WHERE color = '$color' LIMIT 5")->getPoints();
+
+echo "Showing top 5 flags from country $country:" . PHP_EOL;
+print_r($results);
+echo PHP_EOL;
+
+echo "Showing top 5 flags with color $color:" . PHP_EOL;
+print_r($results2);
+
+
+echo PHP_EOL;
+echo sprintf('Executed 1000 inserts in %.2f seconds', $end - $start);
